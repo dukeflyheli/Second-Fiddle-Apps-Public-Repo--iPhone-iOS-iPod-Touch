05afc48dca702e3d17beeeb33077079f3ce279ac
dukeflyheli <business@secondfiddleapps.com>
2010-09-14 00:20:09 -0400

    ADD empty Universal iOS app project

---
+++ iPad/AppDelegate_iPad.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate_iPad.h
+//  SFAClasses
+//
+//  Created by Robert Duke on 9/13/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate_Shared.h"
+
+@interface AppDelegate_iPad : AppDelegate_Shared {
+}
+
+
+@end
+
---
+++ iPhone/AppDelegate_iPhone.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate_iPhone.h
+//  SFAClasses
+//
+//  Created by Robert Duke on 9/13/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate_Shared.h"
+
+@interface AppDelegate_iPhone : AppDelegate_Shared {
+}
+
+
+@end
+
